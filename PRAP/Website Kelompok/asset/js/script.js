7f775ee2e83b3816cad47c25a67887d275626ab8
Nicholas Daniel Wijaya <88551457+nicholaswijaya004@users.noreply.github.com>
2023-10-17 13:53:02 +0700

    Add files via upload

---
+++ PRAP/Website Kelompok/asset/js/script.js
@@ -0,0 +1,26 @@
+$(function () {
+  $(document).scroll(function () {
+    var $header = $("header");
+    $header.toggleClass('navbar-bg', $(this).scrollTop() > $header.height());
+  });
+});
+
+$( document ).ready(function() {
+  let bool = false;
+  $('.hamburger-menu').on('click', function() {
+    if(bool == false) {
+      bool = true;
+      $('.navbar').css('transform', 'translateX(0)');
+      $('.hamburger-menu').fadeOut(function() {
+        $(this).attr('src', 'asset/images/ExitMenu.svg').fadeIn();
+      });
+    } else {
+      bool = false;
+      $('.navbar').css('transform', 'translateX(100%)');
+      $('.hamburger-menu').fadeOut(function() {
+        $(this).attr('src', 'asset/images/HamburgerMenu.svg').fadeIn();
+      });
+    }
+
+  });
+});
